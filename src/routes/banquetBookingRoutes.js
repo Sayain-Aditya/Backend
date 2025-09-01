a5faa0389f787c247eba82e47b24b7589e2f4292
Sayain-Aditya <as4316664@gmail.com>
2025-09-01 16:08:46 +0530

    route

--- src/routes/banquetBookingRoutes.js
+++ src/routes/banquetBookingRoutes.js
@@ -14,8 +14,8 @@ router.post("/create", createBooking);
 router.get("/", getBookings);
 router.get("/pg", getAllPagination);
 router.get('/search',searchBooking)
-router.get("/:id", getBookingById);
-router.delete("/:id", deleteBooking);
-router.put("/:id", updateBooking);
+router.get("/get/:id", getBookingById);
+router.delete("/delete/:id", deleteBooking);
+router.put("/update/:id", updateBooking);
 
 module.exports = router;
